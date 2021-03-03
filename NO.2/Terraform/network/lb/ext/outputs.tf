eaa89b7c608357eb3ed837b74939195009b324f0
Hwang Hee <hw038@naver.com>
2021-03-03 16:33:12 +0900

    Add files via upload

---
+++ NO.2/Terraform/network/lb/ext/outputs.tf
@@ -0,0 +1,16 @@
+output "id" {
+  value = local.lb_id
+}
+
+output "backendpool_id" {
+    value = local.backendpool_id
+}
+
+output "backendpool_id2" {
+    value = local.backendpool_id2
+}
+
+
+output "natpool_id" {
+    value = local.natpool_id
+}
---
+++ NO.2/Terraform/variables.tf
@@ -0,0 +1,80 @@
+variable "rg_name" {
+  description = "The prefix used for all resources in this example"
+  default = "Hands-On-NO.21"
+}
+
+variable "rg_location" {
+  description = "The Azure location where all resources in this example should be created"
+  default = "eastus"
+}
+
+variable "cred" {
+  description = "user ID / Password"
+  type = map
+  default = {
+    id = "azureuser"
+    pw = "Azurexptmxm123"
+  }
+}
+
+
+
+variable "vm01_nic" {
+  description = "The Azure location where all resources in this example should be created"
+  default = "vm01_nic"
+}
+
+variable "vm02_nic" {
+  description = "The Azure location where all resources in this example should be created"
+  default = "vm02_nic"
+}
+
+
+variable "vm03_nic" {
+  description = "The Azure location where all resources in this example should be created"
+  default = "vm03_nic"
+}
+
+variable "vm04_nic" {
+  description = "The Azure location where all resources in this example should be created"
+  default = "vm04_nic"
+}
+
+
+
+
+variable "azure_cidr" {
+  type = map
+  default = {
+    cidr_vnet01 = "10.0.0.0/8"
+    cidr_vnet02 = "192.168.0.0/16"
+    cidr_subnet01 = "10.1.0.0/16"
+    cidr_subnet02 = "192.168.1.0/26"
+  }
+  description = "Azure CIDR for vnet & subnet"
+}
+  
+variable "name" {
+  default = {
+    vnet01 = "vnet01"
+    vnet02 = "vnet02"
+    subnet01 = "subnet01"
+    subnet02 = "subnet02"
+  }
+}  
+
+
+variable "elb_name" {
+  description = "The Azure location where all resources in this example should be created"
+  default = ""
+}
+variable "elb_PIP" {
+  description = "The Azure location where all resources in this example should be created"
+  default = "elb_PIP"
+}
+
+
+variable "elb_lbrule" {
+  description = "The Azure location where all resources in this example should be created"
+  default = "elb_lbrule"
+}
