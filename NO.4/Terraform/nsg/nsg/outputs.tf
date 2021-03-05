2de5465a8815b0c2389d329922fee5f8db73d4e1
Hwang Hee <hw038@naver.com>
2021-03-05 14:40:19 +0900

    Delete NO.4/Terraform/nsg directory

--- NO.4/Terraform/nsg/nsg/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "id" {
-    description = "생성된 모든 network security group 정보를 name: id 형태로 전달"
-
-    value = {
-        for nsg in azurerm_network_security_group.tfmodule:
-        nsg.name => nsg.id
-    }
-}
--- NO.4/Terraform/nsg/nsg_subnet_set/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "info" {
-    description = "[ [subnet_id, network_security_group_id ]....]"
-    value = local.set_id
-}
