d1d636bfc70560b4f723f75fb751b7caebdcf146
Hwang Hee <hw038@naver.com>
2021-03-05 14:40:37 +0900

    Delete NO.4/Terraform/pip directory

--- NO.4/Terraform/pip/pip.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_public_ip" "tfmodule" {
-    count                       = length(var.public_ips)
-    location                    = var.location
-    resource_group_name         = var.resource_group_name    
-    name                        = var.public_ips[count.index][0]
-    sku                         = var.public_ips[count.index][1] == "S" ? "Standard" : "Basic"
-    allocation_method           = var.public_ips[count.index][2] == "D" ? "Dynamic" : "Static"    
-}
