3b35b4c51e1e01680eddf07f58223a245b80c626
Hwang Hee <hw038@naver.com>
2021-03-05 14:39:58 +0900

    Delete NO.4/Terraform/conn directory

--- NO.4/Terraform/conn/bastion/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "id" {
-    description = "생성된 모든 bastion 정보를 name: id 형태로 전달"
-
-    value = {
-        for bastion in azurerm_bastion_host.tfmodule:
-        bastion.name => bastion.id
-    }
-}
