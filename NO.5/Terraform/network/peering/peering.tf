411f859ef558b32e74d9343ba836c9c917a927b4
Hwang Hee <hw038@naver.com>
2021-03-12 16:30:48 +0900

    Add files via upload

--- NO.5/Terraform/network/peering/peering.tf
+++ NO.5/Terraform/network/peering/peering.tf
@@ -3,6 +3,8 @@ resource "azurerm_virtual_network_peering" "peer1to2" {
   resource_group_name       = var.resource_group_name
   virtual_network_name      = var.vnet1name
   remote_virtual_network_id = var.vnet2id
+  allow_forwarded_traffic   = true
+  allow_gateway_transit     = true
 }
 
 resource "azurerm_virtual_network_peering" "peer2to1" {
@@ -10,4 +12,6 @@ resource "azurerm_virtual_network_peering" "peer2to1" {
   resource_group_name       = var.resource_group_name
   virtual_network_name      = var.vnet2name
   remote_virtual_network_id = var.vnet1id
+  allow_forwarded_traffic = true
+  use_remote_gateways       = true
 }
